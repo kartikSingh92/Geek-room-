32521d75f258fb26fa4feaa5bfb14c54cb4f7416
kartikSingh92 <kartik921363@gmail.com>
2025-10-05 03:00:08 +0530

    Python basic ques

---
+++ python basic.py
@@ -0,0 +1,20 @@
+#write a program to input two numbers and print their sum.
+a=int(input("enter 1st number"))
+print("your first number is:",a)
+b=int(input("enter second number"))
+print("your second number is:",b)
+print("sum is",a+b)
+
+#wap to input side of square and print its area
+a=int(input("Side of square:"))
+print("area of square",a*a)
+
+#wap to input 2 floating point numbers and print their average
+num1=float(input("Enter first number:"))
+num2=float(input("Enter second number:"))
+print("Average is:",(num1+num2)/2)
+
+#wap to input 2 int numbers,a and b.Print true if a is greater than or equal to b.if not print false
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+print(a>=b)
